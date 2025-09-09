c246e8f581565d9dc80ef173511a1d07c5bfab73
oono <tacopaz7@outlook.jp>
2025-09-09 18:25:26 +0900

    濱邊

--- KenInteriorShop/src/main/java/jp/ken/interiorShop/domain/EmpRepository/EmpOrderRepository.java
+++ KenInteriorShop/src/main/java/jp/ken/interiorShop/domain/EmpRepository/EmpOrderRepository.java
@@ -1,7 +1,13 @@
 package jp.ken.interiorShop.domain.EmpRepository;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import jp.ken.interiorShop.presentation.EmpFormModel.EmpOrderFormModel;
 
 //担当：濱邊
 //注文管理に関するDBアクセスを担当するリポジトリクラス
@@ -11,9 +17,8 @@ public class EmpOrderRepository {
 	
 	private JdbcTemplate jdbcTemplate;
 	
-	//注文一覧取得用メソッド
-	/*
-	public List<UserOrderFormModel> getOrderList() throws Exception{
+	//注文一覧全件取得用メソッド
+	public List<EmpOrderFormModel> getOrderList() throws Exception{
 		
 		StringBuilder sb = new StringBuilder();
 		sb.append("SELECT");
@@ -23,13 +28,15 @@ public class EmpOrderRepository {
 		sb.append("SELECT");
 		sb.append("SELECT");
 		sb.append("SELECT");
+		String sql = sb.toString();
 		
+		//BeanPropertyRowMapper<>で、SQLの結果セット（ResultSet）をJavaのオブジェクトに自動でマッピング
+		RowMapper<EmpOrderFormModel> mapper = new BeanPropertyRowMapper<>(EmpOrderFormModel.class);
+		//データベースから複数行の結果を取得する
+		List<EmpOrderFormModel> orderList = jdbcTemplate.query(sql,mapper);
 		
-		//List<EmpOrderFormModel> orderList = jdbcTemplate.query(sql,)
-		
-		return 
+		return orderList;
 	}
-	*/
 	
 
 }
