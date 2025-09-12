fffe214db4614557c22a1f5d03ee322eedcfcfec
KEN <KEN@FUK-HP106>
2025-09-12 16:46:07 +0900

    西村-会員情報管理画面の変更機能作成、メールアドレス重複チェック機能の修正

--- KenInteriorShop/src/main/java/jp/ken/interiorShop/presentation/controller/UserInfoController.java
+++ KenInteriorShop/src/main/java/jp/ken/interiorShop/presentation/controller/UserInfoController.java
@@ -1,5 +1,8 @@
 package jp.ken.interiorShop.presentation.controller;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,28 +14,34 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import jp.ken.interiorShop.common.validator.groups.ValidGroupOrder;
+import jp.ken.interiorShop.domain.repository.UserSearchRepository;
 import jp.ken.interiorShop.presentation.formmodel.UserInfoFormModel;
 import jp.ken.interiorShop.presentation.formmodel.UserLoginFormModel;
+import jp.ken.interiorShop.service.UserInfoService;
 
 /*
  * 作成 : 西村
  * 会員情報確認画面
  */
 @Controller
-@SessionAttributes("UserLoginForm")
+@SessionAttributes({"UserLoginForm", "userInfoFormModel"})
 public class UserInfoController {
-
 	
+	private UserInfoService userInfoService;
+	private UserSearchRepository userSearchRepository;
 	
-	public UserInfoController() {
-		
+	public UserInfoController(
+			UserInfoService userInfoService,
+			UserSearchRepository userSearchRepository) {
+		this.userInfoService = userInfoService;
+		this.userSearchRepository = userSearchRepository;
 	}
 	
 	// セッションオブジェクトの生成
-//	@ModelAttribute("UserLoginForm")
-//	public UserLoginFormModel setupUserLoginForm() {
-//		return new UserLoginFormModel();
-//	}
+	@ModelAttribute("userInfoFormModel")
+	public UserInfoFormModel setupUserInfoForm() {
+		return new UserInfoFormModel();
+	}
 	
 	/**
 	 * 会員情報確認画面 GET通信用メソッド
@@ -43,11 +52,13 @@ public class UserInfoController {
 	public String toInfo(SessionStatus status, Model model) {
 		UserLoginFormModel loginForm = (UserLoginFormModel) model.getAttribute("UserLoginForm");
 		// ログイン情報がない場合、会員ログイン画面にリダイレクト
-		if(loginForm.getUserId() == null || loginForm.getUserId().isEmpty()) {
+		if(loginForm == null ||
+				loginForm.getUserId() == null || loginForm.getUserId().isEmpty()) {
 			return "redirect:/user/login";
 		}
-		// ログイン情報を確認画面表示用のFormModelにセット
+		// ログイン情報を確認画面表示用セッション情報にセット
 		UserInfoFormModel infoForm = new UserInfoFormModel();
+		infoForm.setUserId(loginForm.getUserId());				//ID
 		infoForm.setUserName(loginForm.getUserName());			//名前
 		infoForm.setUserKana(loginForm.getUserKana());			//フリガナ
 		infoForm.setUserGender(loginForm.getUserGender());		//性別
@@ -59,7 +70,7 @@ public class UserInfoController {
 		infoForm.setUserPass(loginForm.getUserPass());			//パスワード
 		
 		model.addAttribute("userInfoFormModel", infoForm);
-		
+		model.addAttribute("infoMsg", "お客様の登録情報を表示しています。");
 		return "userInfo";
 	}
 	
@@ -74,48 +85,112 @@ public class UserInfoController {
 	@PostMapping(value = "/user/info")
 	public String infoUser(@Validated(ValidGroupOrder.class) @ModelAttribute UserInfoFormModel infoForm,
 			BindingResult result, Model model) throws Exception {
-		/*
-		 * [退会ボタン押下時]
-		 * 確認ダイアログを表示
-		 * 		はい → 現在ログイン中の会員情報をDBから削除(セッションの情報を参照する)
-		 *			成功 → セッション破棄して会員メインメニュー画面へ
-		 *			失敗 → ロールバック、エラーメッセージを追加
-		 *		いいえ → 本画面を再表示 (「退会処理をキャンセルしました」的なメッセージを入れてもいいかも)
-		 */
+		
 		// バリデーションチェック
 		if(result.hasErrors()) {
+			model.addAttribute("error", "入力エラーがあります。");
 			return "userInfo";
 		}
-		
+		// 変更された値が1つでもあるかチェック
+		UserLoginFormModel loginForm = (UserLoginFormModel) model.getAttribute("UserLoginForm");
+		if(!checkChangeData(infoForm, loginForm)) {
+			model.addAttribute("infoMsg", "変更された項目はありません。");
+			return "userInfo";
+		}
 		/*
-		 * [保存]ボタン押下時
-		 * バリデーションチェック(上記)
-		 * UserInfoFormModelにセッションのログイン情報からID,性別,生年月日を入れてDB更新
-		 *			成功 → 適当なメッセージを表示してセッション情報・画面更新
-		 *			失敗 → ロールバック、エラーメッセージを追加
+		 * メールアドレスのチェック
+		 * ① セッションのログイン情報(UserLoginForm)のメールアドレスと同一かチェック(同一なら②はスキップ)
+		 * ② 入力メールアドレスを引数として会員テーブル検索、検索結果が0件かチェック
+		 *   どちらも満たさない場合、 エラーメッセージを追加
 		 */
+		String infoMail = infoForm.getUserMail();
+		if(!infoMail.equals(loginForm.getUserMail()) &&
+				userSearchRepository.chkUserByMail(infoMail) != 0) {
+			model.addAttribute("error", "入力エラーがあります。");
+			model.addAttribute("mailError", "他のお客様によってすでに登録済みのメールアドレスです");
+			return "userInfo";
+		}
+		// DB更新処理
+		int numberOfRow = userInfoService.updateUser(infoForm);
 		
+		if(numberOfRow == 0) {
+			model.addAttribute("error", "変更に失敗しました");
+			return "userInfo";
+		}
+		// セッション情報(UserLoginForm)を更新
+		loginForm.setUserName(infoForm.getUserName());			//名前
+		loginForm.setUserKana(infoForm.getUserKana());			//フリガナ
+		loginForm.setUserPost(infoForm.getUserPost());			//郵便番号
+		loginForm.setUserAddress(infoForm.getUserAddress());	//住所
+		loginForm.setUserPhone(infoForm.getUserPhone());		//電話番号
+		loginForm.setUserMail(infoForm.getUserMail());			//メールアドレス
+		loginForm.setUserPass(infoForm.getUserPass());			//パスワード
 		
-		// 確認用として一旦メインメニューに飛ばす
-		return "redirect:/user/main";
+		model.addAttribute("UserLoginForm", loginForm);
+		// 変更完了メッセージ
+		model.addAttribute("infoMsg", "お客様の登録情報を変更しました。");
+		
+		return "userInfo";
+	}
+	
+	/**
+	 * 退会 POST通信用メソッド
+	 * @param loginForm
+	 * @param result
+	 * @param model
+	 * @return 
+	 * @throws Exception
+	 */
+	@PostMapping(value = "/user/info/delete")
+	public String deleteUser(SessionStatus status, Model model) throws Exception {
+
+		// ポップアップウィンドウ表示
+		System.setProperty("java.awt.headless", "false");
+		JFrame frame = new JFrame();
+		frame.setAlwaysOnTop(true);		//前面に表示させるための処理
+		int result = JOptionPane.showConfirmDialog(frame, "KEN interior Shopを退会します。\n"
+						+ "本当によろしいですか？\n" + "", "退会確認",
+						JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+		frame.setAlwaysOnTop(false);
+		if(result != JOptionPane.YES_OPTION) {
+			// OKを押下しなかった場合
+			model.addAttribute("infoMsg", "退会をキャンセルしました。");
+			return "userInfo";
+		}
+		// 退会処理を行う
+		// SQL-DELETE文を実行
+//		UserLoginFormModel loginForm = (UserLoginFormModel) model.getAttribute("UserLoginForm");
+//		int numberOfRow = userInfoService.updateUser(loginForm);
+		
+//		if(numberOfRow == 0) {
+//			model.addAttribute("error", "退会処理中にエラーが発生しました");
+//			return "userInfo";
+//		}
+		System.out.println("退会処理 (ダミーメッセージ)");
+		// ポップアップウィンドウ表示
+		System.setProperty("java.awt.headless", "false");
+		frame.setAlwaysOnTop(true);		//前面に表示させるための処理
+		JOptionPane.showMessageDialog(frame, "退会処理が完了しました。");
+		frame.setAlwaysOnTop(false);
+		
+		status.setComplete();
+		return "redirect:/user/login";
 	}
 	
 	/* 入力値変更確認メソッド
-	 * true : 変更なし | false : 変更あり
-	 * 同一メールアドレスはバリデーションチェック時点で弾かれており、
-	 * バリデーションチェックでエラーなし ＝ 最低でもメールアドレスは変更されている
-	 * となるため、不要な処理。念のためコメントアウトで残す
+	 * true : 変更あり | false : 変更なし
+	 * 1つでも変更があれば、true
 	 */ 
-//	public boolean checkChangeData(UserInfoFormModel infoForm, UserLoginFormModel loginForm) {
-//		if(infoForm.getUserName() == loginForm.getUserName()
-//				&& infoForm.getUserKana() == loginForm.getUserKana()
-//				&& infoForm.getUserPost() == loginForm.getUserPost()
-//				&& infoForm.getUserAddress() == loginForm.getUserAddress()
-//				&& infoForm.getUserPhone() == loginForm.getUserPhone()
-//				&& infoForm.getUserMail() == loginForm.getUserMail()
-//				&& infoForm.getUserPass() == loginForm.getUserPass()) {
-//			return true;
-//		}
-//		return false;
-//	}
+	public boolean checkChangeData(UserInfoFormModel infoForm, UserLoginFormModel loginForm) {
+		if(infoForm.getUserName().equals(loginForm.getUserName())
+				&& infoForm.getUserKana().equals(loginForm.getUserKana())
+				&& infoForm.getUserPost().equals(loginForm.getUserPost())
+				&& infoForm.getUserAddress().equals(loginForm.getUserAddress())
+				&& infoForm.getUserPhone().equals(loginForm.getUserPhone())
+				&& infoForm.getUserMail().equals(loginForm.getUserMail())
+				&& infoForm.getUserPass().equals(loginForm.getUserPass())) {
+			return false;
+		}
+		return true;
+	}
 }
